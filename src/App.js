9669a06d3034b174fb43f20d997965ecb9e038b0
Miłosz Kaczmarek <miloszkaczmarek@hotmail.com>
2019-12-30 00:02:36 +0100

    Install react router

--- src/App.js
+++ src/App.js
@@ -1,50 +1,33 @@
 import React, {useEffect, useState} from 'react';
 import Recipe from './Recipe';
+import Recipes from './Recipes';
 import './App.css';
+import Nav from './Nav';
+import About from './About';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
 
-  const APP_ID = '7378c10c';
-  const APP_KEY = '8fbe26a82da90900f62afe482714ad9b';
-
-  const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('chicken');
-
-  useEffect(() => {
-    getRecipes();
-  }, [query])
-
-  const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-    const data = await response.json();
-    setRecipes(data.hits);
-    console.log(data.hits)
-  }
-
-  const updateSearch = e => {
-    setSearch(e.target.value);
-  }
-
-  const getSearch = e => {
-    e.preventDefault();
-    setQuery(search);
-    setSearch('');
-  }
-
   return (
-    <div className="App">
-      <form onSubmit={getSearch} className="search-form">
-        <input className="search-bar" type="text" value={search} onChange={updateSearch} placeholder="Write ingredient" />
-        <button className="search-button">Search</button>
-      </form>
-      <div className="recipes">
-        {recipes.map(recipe => (
-          <Recipe key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image} ingredients={recipe.recipe.ingredients}/>
-        ))}
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+        <Route path="/" exact component={ Home } />
+        <Route path="/about" component={ About } />
+        <Route path="/recipes" component={ Recipes } />
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
+}
+
+const Home = () => {
+  return(
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/Nav.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
+function Nav(){
+
+    const navStyle = {
+        color:'black'
+    }
+
+    return(
+        <nav>
+            <Link to="/">
+                <h3>Logo</h3>
+            </Link>
+            <ul>
+                <Link style={navStyle} to="/about">
+                    <li>About</li>
+                </Link>
+                <Link style={navStyle} to="/recipes">
+                    <li>Recipes</li>
+                </Link>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
